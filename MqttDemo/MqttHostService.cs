abebc1a68f2714828ffb947a1243ddf7d323efda
SunXiaobei <1371815174@qq.com>
2022-09-05 23:22:40 +0800

    创建服务类

---
+++ MqttDemo/MqttHostService.cs
@@ -0,0 +1,20 @@
+﻿namespace MqttDemo
+{
+    public class MqttHostService : IHostedService, IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotSupportedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
