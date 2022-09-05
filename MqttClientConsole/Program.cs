41ec6b32cf88b6ea5d971713cffa545d4758db59
SunXiaobei <1371815174@qq.com>
2022-09-06 07:54:35 +0800

    创建控制台客户端

---
+++ MqttClientConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MqttClientConsole;
+Console.WriteLine("Hello, World!");
+
+MqttClientService mqttClientService = new MqttClientService();
+mqttClientService.MqttClientStart();
+
+Console.ReadLine();
